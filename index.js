b318272586aba15889310d1a700741da06aeffe1
Kaung Htet <72309682+Kaung-H-H@users.noreply.github.com>
2022-10-03 17:32:33 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)}
 
-    function myFunction() {
-    var x = document.getElementById("myText").value;
+function myFunction() {
+var x = document.getElementById("myText").value;
 
-    if (x > 20){
-        document.getElementById("error").style.visibility = "visible"
-        navigator.vibrate(200)
-    }else{
-        document.getElementById("error").style.visibility = "hidden"
-
-
+if (x > 20){
+    document.getElementById("error").innerHTML = "!!The maximum number must be 20"
+    document.getElementById("error").style.visibility = "visible"
+    document.getElementById("minValue").innerHTML = ""
+    document.getElementById("maxValue").innerHTML =  ""
+    document.getElementById("mainValue").innerHTML =  ""
+}else if(x<1){
+    document.getElementById("error").innerHTML = "!!The minimum number must be 1"
+    document.getElementById("error").style.visibility = "visible"
+    document.getElementById("minValue").innerHTML = ""
+    document.getElementById("maxValue").innerHTML =  ""
+    document.getElementById("mainValue").innerHTML =  ""
+}else{
+    document.getElementById("error").style.visibility = "hidden"
     let zeroString = "0"
     let zeroStringData = zeroString.repeat(x-1)+"1" 
     document.getElementById("minValue").innerHTML = zeroStringData
